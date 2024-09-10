fa99e9d247ad6c200710d5ae785e660f496ab6ec
edi.brum <edineiabrum.edi@gmail.com>
2024-09-10 19:44:30 -0300

    adicionando projeto inicial do initializr start.spring.io

---
+++ src/main/java/br/com/exemplo/testai/TestaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.testai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestaiApplication.class, args);
+	}
+
+}

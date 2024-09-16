c986ced77daf13a4a06184766346f2c6ff935b36
edi.brum <edineiabrum.edi@gmail.com>
2024-09-16 17:46:06 -0300

    ajustes para conexao com banco postgresql, e criacao de registros iniciais nas tabelas

--- src/main/java/br/com/exemplo/testai/TestaiApplication.java
+++ src/main/java/br/com/exemplo/testai/TestaiApplication.java
@@ -1,12 +1,27 @@
 package br.com.exemplo.testai;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import jakarta.transaction.Transactional;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TestaiApplication implements CommandLineRunner {
+
+	@Bean
+	public OpenAPI springTestaiAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Testai Application")
+						.description("Documentação da Api do Easymbark")
+						.version("v1.0.0"));
+	}
+
+	@Bean
+	RestTemplate restTeamplate() { return new RestTemplate(); }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestaiApplication.class, args);
